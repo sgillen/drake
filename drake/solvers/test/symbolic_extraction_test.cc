2e835edecca325b9dcbfb548d87a495f278d612c
Eric Cousineau <eric.cousineau@tri.global>
2017-04-28 19:42:30 -0400

    Can recreate using this code

--- drake/solvers/test/symbolic_extraction_test.cc
+++ drake/solvers/test/symbolic_extraction_test.cc
@@ -81,34 +81,38 @@ namespace {
 // }
 
 GTEST_TEST(SymbolicExtraction, AppendToVector) {
-  Variable x("x");
-  Variable y("y");
-  //  Expression e = x + y;
-  VectorXDecisionVariable vars_expected(2);
-  vars_expected << x, y;
-  Variable z("z");
+//  Variable x("x");
+//  Variable y("y");
+//  //  Expression e = x + y;
+//  VectorXDecisionVariable vars_expected(2);
+//  vars_expected << x, y;
+//  Variable z("z");
+//
+//  VectorXDecisionVariable setup(3);
+//  setup << vars_expected, z;
+//
+////  vars_expected.conservativeResize(3);
+////  vars_expected(2) = z;
+//  // internal::AppendToVector(z, &vars_expected);
+//
+//  Variable w;
+//  std::cout << w.get_name() << " " << w.get_id() << std::endl;
+//  w = z;
+//  std::cout << w.get_name() << " " << w.get_id() << std::endl;
+//
+//  vector<Variable> vars2 = {x, y};
+//  vars2.resize(3);
+//  vars2.push_back(z);
+//
+//  VectorXd f(2);
+//  f << 1, 2;
+//  f.conservativeResize(3);
+//  f(2) = 3;
+//  internal::AppendToVector(3, &f);
 
-  VectorXDecisionVariable setup(3);
-  setup << vars_expected, z;
-
-//  vars_expected.conservativeResize(3);
-//  vars_expected(2) = z;
-  // internal::AppendToVector(z, &vars_expected);
-
-  Variable w;
-  std::cout << w.get_name() << " " << w.get_id() << std::endl;
-  w = z;
-  std::cout << w.get_name() << " " << w.get_id() << std::endl;
-
-  vector<Variable> vars2 = {x, y};
-  vars2.resize(3);
-  vars2.push_back(z);
-
-  VectorXd f(2);
-  f << 1, 2;
-  f.conservativeResize(3);
-  f(2) = 3;
-  internal::AppendToVector(3, &f);
+  VectorX<string> g(2);
+  g << "x", "y";
+  internal::AppendToVector("z", &g);
 }
 
 //GTEST_TEST(SymbolicExtraction, ExtractAndAppend) {
