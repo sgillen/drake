d43b850231a1a99c99275f49a1eb924c510222a1
Eric Cousineau <eric.cousineau@tri.global>
2017-06-13 14:36:03 -0400

    Trying to figure out how to get tracking a little tighter.

--- drake/examples/kuka_iiwa_arm/dev/monolithic_pick_and_place/show_sparsity.m
+++ drake/examples/kuka_iiwa_arm/dev/monolithic_pick_and_place/show_sparsity.m
@@ -1,11 +1,17 @@
 function [] = show_sparsity(index, Q, f, K)
-
 sfigure(index + 1);
-subplot(1, 2, 1);
-spy(Q);
-subplot(1, 2, 2);
-spy(f);
+subplot(1, 4, [1 3]);
+plot_mat(Q);
+subplot(1, 4, 4);
+plot_mat(f);
 fprintf('K[%d] = %g\n', index, K);
 drawnow();
+end
 
+function [] = plot_mat(A)
+colormap('gray');
+image(A, 'CDataMapping','scaled');
+colorbar();
+axis('equal');
+set(gca, 'Visible', 'off');
 end
