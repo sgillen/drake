298e2c7b838a26e92eb790c664af31cb69c13660
Eric Cousineau <eric.cousineau@tri.global>
2017-06-13 21:26:56 -0400

    Need to minimize lift height

--- drake/manipulation/estimators/dev/ManipulationTracker.cpp
+++ drake/manipulation/estimators/dev/ManipulationTracker.cpp
@@ -571,9 +571,13 @@ void ManipulationTracker::update(const Eigen::VectorXd &q_sub, const VectorSlice
   // (x - x_pred)' (covar_pred^(-1)) (x - x_pred)
   // which can be rewritten in our form as 
   covar_pred = covar_pred.inverse();
-//  Q += covar_pred;
-//  f -= x_pred.transpose() * covar_pred;
-//  K += 2 * x_pred.transpose() * covar_pred * x_pred;
+  bool ignore_covar_pred = false;
+
+  if (!ignore_covar_pred) {
+    Q += covar_pred;
+    f -= x_pred.transpose() * covar_pred;
+    K += 2 * x_pred.transpose() * covar_pred * x_pred;
+  }
 
   // do measurement update proper if we had any useful costs
   if (fabs(K) > 0.0){
