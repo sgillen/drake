da976e6d97b25ceb788f73f4e678e9f8d74bd2d0
Eric Cousineau <eric.cousineau@tri.global>
2017-09-12 16:55:37 -0400

    Teach PassThrough to handle AbstractValue's.

--- drake/systems/primitives/pass_through-inl.h
+++ drake/systems/primitives/pass_through-inl.h
@@ -14,21 +14,66 @@ namespace systems {
 // TODO(amcastro-tri): remove the size parameter from the constructor once
 // #3109 supporting automatic sizes is resolved.
 template <typename T>
-PassThrough<T>::PassThrough(int size) : VectorSystem<T>(size, size) { }
+PassThrough<T>::PassThrough(int size) {
+  BasicVector<T> model_value(size);
+  this->DeclareVectorInputPort(model_value);
+  this->DeclareVectorOutputPort(
+      model_value, &PassThrough::DoCalcVectorOutput);
+}
+
+template <typename T>
+PassThrough<T>::PassThrough(const AbstractValue& model_value)
+     : abstract_model_value_(model_value.Clone()) {
+  // TODO(eric.cousineau): Remove value parameter from the constructor once
+  // the equivalent of #3109 for abstract values is also resolved.
+  this->DeclareAbstractInputPort(model_value);
+  // Use the std::function<> overloads to work with `AbstractValue` type
+  // directly and maintain type erasure.
+  namespace sp = std::placeholders;
+  auto allocate_abstract_value = [&](const Context<T>&) {
+    return abstract_model_value_->Clone();
+  };
+  this->DeclareAbstractOutputPort(
+      allocate_abstract_value,
+      std::bind(&PassThrough::DoCalcAbstractOutput, this, sp::_1, sp::_2));
+}
 
 template <typename T>
 void PassThrough<T>::DoCalcVectorOutput(
-    const Context<T>&,
-    const Eigen::VectorBlock<const VectorX<T>>& input,
-    const Eigen::VectorBlock<const VectorX<T>>& state,
-    Eigen::VectorBlock<VectorX<T>>* output) const {
-  unused(state);
-  *output = input;
+      const Context<T>& context,
+      BasicVector<T>* output) const {
+  DRAKE_ASSERT(!is_abstract());
+  const BasicVector<T>& input = *this->EvalVectorInput(context, 0);
+  output->SetFrom(input);
+}
+
+template <typename T>
+void PassThrough<T>::DoCalcAbstractOutput(const Context<T>& context,
+                                          AbstractValue* output) const {
+  DRAKE_ASSERT(is_abstract());
+  const AbstractValue& input =
+      *this->EvalAbstractInput(context, 0);
+  output->SetFrom(input);
+}
+
+template <typename T>
+optional<bool> PassThrough<T>::DoHasDirectFeedthrough(
+    int input_port, int output_port) const {
+  DRAKE_DEMAND(input_port == 0);
+  DRAKE_DEMAND(output_port == 0);
+  // By definition, a pass-through will have direct feedthrough, as the
+  // output only depends on the input.
+  return true;
 }
 
 template <typename T>
 PassThrough<symbolic::Expression>* PassThrough<T>::DoToSymbolic() const {
-  return new PassThrough<symbolic::Expression>(this->get_input_port().size());
+  if (!is_abstract()) {
+    return new PassThrough<symbolic::Expression>(this->get_input_port().size());
+  } else {
+    // Return `nullptr` to enable control to reach `DoHasDirectFeedthrough`.
+    return nullptr;
+  }
 }
 
 }  // namespace systems
