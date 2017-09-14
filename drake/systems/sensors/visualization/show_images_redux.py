79f24bedb98a2c4daa06ef425e87c468c08debad
Eric Cousineau <eric.cousineau@tri.global>
2017-09-13 22:46:06 -0400

    Refactor and document.

--- drake/systems/sensors/visualization/show_images_redux.py
+++ drake/systems/sensors/visualization/show_images_redux.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env directorPython
 
-# TODO: Make a simple camera viewing widget.
+"""
+This provides a simple image viewer widget, based upon:
+*   director.cameraview.CameraImageView
+*   app/ddBotImageQueue.cpp
+"""
 
 import sys
 import argparse
@@ -28,6 +32,9 @@ from PythonQt import QtGui
 verbose = True
 
 class ImageHandler(object):
+    """
+    Generic handler to update an image for `ImageWidget`.
+    """
     def update_image(self, image):
         """
         This should update `image` in-place, either using `DeepCopy`, or by
@@ -35,18 +42,22 @@ class ImageHandler(object):
         If the image is updated, this should return True.
         Otherwise, it should return False.
         """
-        raise Exception("Please implement")
+        raise Exception("Not implemented")
 
-# This is more like director...CameraImageView than ImageWidget
+
 class ImageWidget(object):
+    """
+    Wrapper for displaying vtkImageData on a director-style view.
+
+    @note This is more like director's `CameraImageView` than its `ImageWidget`.
+    """
     def __init__(self, image_handler):
         self.name = 'Image View'
         self.view = PythonQt.dd.ddQVTKWidgetView()
         self.image_handler = image_handler
 
         self.image = vtk.vtkImageData()
-        self.initialized = False
-        self.prev_shape = (0, 0, 0)
+        self.prev_attrib = None
 
         # Initialize the view.
         self.view.installImageInteractor()
@@ -75,25 +86,25 @@ class ImageWidget(object):
         if not has_new:
             return
 
-        if not self.initialized:
-            # Ensure it is visible.
-            self.initialized = True
-            self.imageActor.SetVisibility(True)
-
-        cur_shape = get_vtk_image_shape(self.image)
-        if self.prev_shape != cur_shape:
+        cur_attrib = get_vtk_image_attrib(self.image)
+        if self.prev_attrib != cur_attrib:
+            if prev_attrib is None:
+                # Ensure it is visible.
+                self.imageActor.SetVisibility(True)
             # Fit image to view.
-            self.fit_image_to_view()
+            self.on_new_image_attrib()
             # Update.
-            self.prev_shape = cur_shape
+            self.prev_attrib = cur_attrib
 
         self.view.render()
 
-    def fit_image_to_view(self):
+    def on_new_image_attrib(self):
         # Must render first.
         self.view.render()
 
-        # No idea why this is needed, but it works.
+        # Fit image to view.
+        # TODO(eric.cousineau): No idea why this is needed, but it works
+        # - sorta.
         camera = self.view.camera()
         camera.ParallelProjectionOn()
         camera.SetFocalPoint(0,0,0)
@@ -110,11 +121,14 @@ class ImageWidget(object):
 
 
 class ImageArrayWidget(object):
-    def __init__(self, image_widgets):
+    """
+    Provides a widget to show images from multiple `ImageHandler`s.
+    """
+    def __init__(self, handlers):
         super(ImageArrayWidget, self).__init__()
         # Create widget and layouts
         self.widget = QtGui.QWidget()
-        self.image_widgets = image_widgets
+        self.image_widgets = map(ImageWidget, handlers)
         self.layout = QtGui.QHBoxLayout(self.widget)
         for image_widget in self.image_widgets:
             self.layout.addWidget(image_widget.view)
@@ -130,10 +144,17 @@ class ImageArrayWidget(object):
         self.widget.show()
 
 kDefaultChannel = "DRAKE_RGBD_CAMERA_IMAGES"
-kDefaultFrameNames = ["color", "depth"]
 
 class DrakeLcmImageViewer(object):
+    """
+    Visualize Drake LCM Images.
+    """
     def __init__(self, channel = kDefaultChannel, frame_names = None):
+        """
+        @param frame_names
+            If None, this will defer creating the subscriber and
+            widgets until the first message has been received.
+        """
         self.channel = channel
         if frame_names is None:
             self._create_deferred()
@@ -144,27 +165,23 @@ class DrakeLcmImageViewer(object):
         self.frame_names = frame_names
         self.subscriber = LcmImageArraySubscriber(
             self.channel, self.frame_names)
-
-        handlers = self.subscriber.get_handlers()
-
-        image_widgets = []
-        for handler in handlers:
-            image_widgets.append(ImageWidget(handler))
-        self.array_widget = ImageArrayWidget(image_widgets)
+        self.widget = ImageArrayWidget(self.subscriber.get_handlers())
 
     def _create_deferred(self):
         # Defer creating viewer until we have a message.
         def callback(msg):
             frame_names = [image.header.frame_name for image in msg.images]
-            print("Received on '{}'. Initializing frames {}".format(
-                self.channel, frame_names))
+            print("DrakeLcmImageViewer: Received on '{}', frame_names = {}"
+                .format(self.channel, frame_names))
             self._init_full(frame_names)
             lcmUtils.removeSubscriber(self._sub)
-        print("Deferring creation until '{}' is received".format(self.channel))
+        print("DrakeLcmImageViewer: Defer setup until '{}' is received".format(
+            self.channel))
         self._sub = lcmUtils.addSubscriber(
             self.channel, rl.image_array_t, callback)
 
 def create_image(w, h, num_channels = 1, dtype=np.uint8):
+    """ Creates a VTK image. """
     image = vtk.vtkImageData()
     image.SetWholeExtent(0, w - 1, 0, h - 1, 0, 0)
     image.SetExtent(image.GetWholeExtent())
@@ -176,7 +193,10 @@ def create_image(w, h, num_channels = 1, dtype=np.uint8):
     return image
 
 def create_image_if_needed(w, h, num_channels, dtype, image_in):
-    # If possible, use existing memory.
+    """
+    Creates a VTK image if `image_in` is not compatible with the desired
+    attributes. Otherwise, passes `image_in` through.
+    """
     if image_in is not None:
         dim = (w, h, num_channels)
         attrib_out = (dim, dtype)
@@ -186,10 +206,18 @@ def create_image_if_needed(w, h, num_channels, dtype, image_in):
     # Otherwise, create new image.
     return create_image(w, h, num_channels, dtype)
 
+def vtk_image_to_numpy(image):
+    """ Gets a properly shaped NumPy view of a VTK image's memory. """
+    data = vtk_to_numpy(image.GetPointData().GetScalars())
+    data.shape = get_vtk_image_shape(image)
+    return data
+
 def get_vtk_image_shape(image):
     """
-    vtkImageData.GetDimensions() returns (w, h, 1),
-    not (w, h, GetNumberOfScalarComponents()).
+    Gets `(w, h, num_channels)`.
+
+    @note `vtkImageData.GetDimensions()` returns `(w, h, num_arrays)`, where
+    typically `num_arrays == 1 != num_channels`.
     """
     w, h = image.GetDimensions()[:2]
     num_channels = image.GetNumberOfScalarComponents()
@@ -197,14 +225,15 @@ def get_vtk_image_shape(image):
 
 def get_vtk_image_attrib(image):
     """
-    Return ((w, h, num_channels), dtype).
+    Gets `((w, h, num_channels), dtype)` to check if an existing image is
+    compatible.
     """
     data = vtk_image_to_numpy(image)
     return (data.shape, data.dtype)
 
 def decode_image_t(msg, image_in = None):
     """
-    Decode image_t to vtkImageData, using an existing image if it is compatible.
+    Decodes `image_t` to vtkImageData, using an existing image if it is compatible.
     """
     rli = rl.image_t
     w = msg.width
@@ -239,12 +268,11 @@ def decode_image_t(msg, image_in = None):
     # Return image.
     return image
 
-def vtk_image_to_numpy(image):
-    data = vtk_to_numpy(image.GetPointData().GetScalars())
-    data.shape = get_vtk_image_shape(image)
-    return data
-
 class LcmImageHandler(ImageHandler):
+    """
+    Provides a connection between `LcmImageArraySubscriber`, for a specific
+    image frame, and `ImageWidget`.
+    """
     def __init__(self):
         self.image = None  # vtkImageData
         self.utime = 0
@@ -253,11 +281,18 @@ class LcmImageHandler(ImageHandler):
         self.prev_utime = 0
 
     def receive_message(self, msg):
+        """
+        Receives and decodes `image_t` message into `vtkImageData`.
+        """
+        # TODO(eric.cousineau): Consider moving decode logic.
         with self.lock:
             self.utime = msg.header.utime
             self.image = decode_image_t(msg, self.image)
 
     def update_image(self, image_out):
+        """
+        @see ImageHandler.update_image
+        """
         with self.lock:
             if self.utime == self.prev_utime:
                 return False
@@ -269,7 +304,11 @@ class LcmImageHandler(ImageHandler):
             image_out.DeepCopy(self.image)
         return True
 
+
 class LcmImageArraySubscriber(object):
+    """
+    Provides a connection between the LCM channel and 
+    """
     def __init__(self, channel = kDefaultChannel,
                  frame_names = kDefaultFrameNames):
         self.channel = channel
@@ -281,6 +320,8 @@ class LcmImageArraySubscriber(object):
             channel, rl.image_array_t, self.on_message)
 
     def get_handlers(self):
+        """ Returns ordered list of handlers. """
+        # TODO(eric.cousineau): Consider just using `OrderedDict`.
         return map(self.handlers.get, self.frame_names)
 
     def on_message(self, msg):
@@ -301,13 +342,17 @@ class LcmImageArraySubscriber(object):
                 issues.append("Got extra image '{}'".format(frame_name))
 
         if issues:
-            print("For image channel '{}' ({} images), with frames {}:".format(
-                self.channel, msg.num_images, msg_frame_names))
+            print("LcmImageArraySubscriber: For image channel '{}' ({} images), with frames {}:"
+                .format(self.channel, msg.num_images, msg_frame_names))
             for issue in issues:
                 print("  {}".format(issue))
 
 
 class TestImageHandler(ImageHandler):
+    """
+    Provides a simple test image handler.
+    This just shows an animated gradient, either in color or in grayscale.
+    """
     def __init__(self):
         self.do_color = False
         self.start_time = time.time()
@@ -337,8 +382,7 @@ class TestImageHandler(ImageHandler):
         w, h = data.shape[:2]
         
         x = np.matlib.repmat(
-            np.linspace(0, 1, w).reshape(-1, 1),
-            1, h)
+            np.linspace(0, 1, w).reshape(-1, 1), 1, h)
         T = 1
         w = 2 * math.pi / T
         s = (math.sin(w * t) + 1) / 2.
@@ -358,15 +402,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_image', action='store_true',
         help = "Use a test image handler.")
-    parser.add_argument('--channel', type=str, default=kDefaultChannel)
+    parser.add_argument('--channel', type=str, default=kDefaultChannel,
+        help = "Channel for LCM.")
     parser.add_argument('--frame_names', type=str, nargs='+', default=None,
-        help="By default, will populate with first set of frames. Otherwise, can be manually specified")
+        help = "By default, will populate with first set of frames. Otherwise, can be manually specified")
     args, _ = parser.parse_known_args()
 
     if args.test_image:
-        image_viewer = ImageArrayWidget([
-            ImageWidget(TestImageHandler())
-            ])
+        image_viewer = ImageArrayWidget([TestImageHandler()])
     else:
         image_viewer = DrakeLcmImageViewer(args.channel, args.frame_names)
 
