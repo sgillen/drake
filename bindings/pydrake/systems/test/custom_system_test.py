35bae6541f85ef08389d2cdf207635406f3c024f
Eric Cousineau <eric.cousineau@tri.global>
2017-12-13 18:10:17 -0500

    Ah shooot.

--- bindings/pydrake/systems/test/custom_system_test.py
+++ bindings/pydrake/systems/test/custom_system_test.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function
 
+import copy
 import unittest
 import numpy as np
 
@@ -30,18 +31,23 @@ class CustomAdder(LeafSystem):
 
 class TestCustomSystem(unittest.TestCase):
     def test_custom_adder(self):
-        system = CustomAdder(2, 3)
+        system = self._create_system()
         self._test_execution(system)
-        self._test_simulation(system)
+        # WARNING: At present, `Simulator` takes ownership, and will destroy
+        # `system`. To sidestep this, we will clone the existing system.
+        self._test_simulation(copy.copy(system))
         # Ensure that, even though `simulator` has been destroyed,
         # we can still access `system`.
         self._test_execution(self, system)
+
+    def _create_system(self):
+        return CustomAdder(2, 3)
 
     def _setup_inputs(self, context):
         context.FixInputPort(0, BasicVector([1, 2, 3]))
         context.FixInputPort(1, BasicVector([4, 5, 6]))
 
-    def _test_execution(system):
+    def _test_execution(self, system):
         context = system.CreateDefaultContext()
         self._setup_inputs(context)
         assert context.get_num_input_ports() == 2
@@ -51,10 +57,9 @@ class TestCustomSystem(unittest.TestCase):
         self.assertTrue(np.allclose([5, 7, 9], value))
 
     def _test_simulation(self, system):
-        pass
-
-    def test_lifetime(self):
-        simulator = Simulator(CustomAdder(2, 3))
+        simulator = Simulator(system)
+        # Get rid of reference.
+        del system
         simulator.Initialize()
         context = simulator.get_mutable_context()
         self._setup_inputs(context)
@@ -62,6 +67,11 @@ class TestCustomSystem(unittest.TestCase):
         # Ensure that we have the outputs we want.
         context.get_output()
         self.assertTrue(False)
+
+    def test_lifetime(self):
+        # Ensure that the Python subclass stays alive when all Python
+        # references disappear.
+        self._test_simulation(self._create_system())
 
 
 if __name__ == '__main__':
