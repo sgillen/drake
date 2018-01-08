5b1413164b9c07cda58ab3c2b6ab23f576d64e05
Eric Cousineau <eric.cousineau@tri.global>
2018-01-08 17:28:26 -0500

    Can run the test...

--- tools/external_data/workspace_test.sh
+++ tools/external_data/workspace_test.sh
@@ -1,1 +1,10 @@
-echo $@
+#!/bin/bash
+set -e -u -x
+
+cd "${1}"
+# Make sure we're where we want.
+grep 'bazel_external_data_pkg' WORKSPACE
+# Run all the tests.
+bazel test //...
+# Ensure that we wipe out symlinks so that it doesn't fudge any upstream stuff.
+./clear.sh
