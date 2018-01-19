cb81a0ce12fef3f07c11a7c507508bcd6f6147e7
Eric Cousineau <eric.cousineau@tri.global>
2018-01-19 14:46:05 -0500

    WIP: Start binding images.

--- bindings/pydrake/systems/sensors_py.cc
+++ bindings/pydrake/systems/sensors_py.cc
@@ -1,33 +1,120 @@
+#include <vector>
+#include <string>
+
 #include <pybind11/eigen.h>
-#include <pybind11/eval.h>
-#include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include "drake/bindings/pydrake/util/type_pack.h"
 #include "drake/systems/sensors/image.h"
 #include "drake/systems/sensors/rgbd_camera.h"
 #include "drake/systems/sensors/pixel_types.h"
 
 namespace py = pybind11;
 
+template <typename T, T Value>
+using constant = std::integral_constant<T, Value>;
+
+template <typename T, T ... Values>
+using constant_pack = drake::type_pack<constant<T, Values>...>;
+
+namespace drake {
+namespace pydrake {
+
+// Allow `Type` or `const Type`
+template <typename ImageT, typename T>
+py::object GetImageArray() {
+  // Create flat array.
+  Eigen::Map<VectorX<T>> data(self->at(0, 0), self->size());
+  // Reshape with NumPy.
+  py::object array =
+      py::cast(data).attr("reshape")(
+          self->width(), self->height(), self->size());
+  return array;
+}
+
+}  // namespace
+
 PYBIND11_MODULE(sensors, m) {
   // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
-  using namespace drake::systems;
+  using namespace drake::pydrake;
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake;
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake::systems::sensors;
+
   auto py_iref = py::return_value_policy::reference_internal;
 
   m.doc() = "Bindings for the sensors portion of the Systems framework.";
 
   using T = double;
 
-  py::enum_<PixelType>(m, "PixelType")
-    .value("kRgb8U", kRgb8U)
-    .value("kBgr8U", kBgr8U)
-    .value("kRgba8U", kRgba8U)
-    .value("kBgra8U", kBgra8U)
-    .value("kGrey8U", kGrey8U)
-    .value("kDepth16U", kDepth16U)
-    .value("kDepth32F", kDepth32F)
-    .value("kLabel16I", kLabel16I);
-
+  // Expose only types that are used.
+  py::enum_<PixelFormat>(m, "PixelFormat")
+    .value("kRgba", PixelFormat::kRgba)
+    .value("kDepth", PixelFormat::kDepth)
+    .value("kLabel", PixelFormat::kLavel);
   // Expose image traits.
+  {
+    py::enum_<PixelType> pixel_type(m, "PixelType");
+    vector<string> names = {
+        "kRgba8U",
+        "kDepth32F",
+        "kLabel16I",
+    };
+    using Pack = constant_pack<PixelType,
+        PixelType::kRgba8U,
+        PixelType::kDepth32F,
+        PixelType::kLabel16I>;
+    // Simple constexpr for-loop.
+    int i = 0;
+    auto iter = [&](auto tag) {
+      constexpr PixelType Value = tag::template type<0>::value;
+      using TraitsT = ImageTraits<PixelType;
+      using T = typename TraitsT::ChannelType;
+      // Add definition.
+      pixel_type.value(names[i], value);
+
+      // Add traits.
+      // TODO(eric.cousineau): Use C++ template.
+      py::class_<TraitsT> traits(m, ("ImageTraits_" + names[i]).c_str());
+      traits.attr("ChannelType") = GetPyParam<T>()[0];
+      traits.attr("kNumChannels") = TraitsT::kNumChannels;
+      traits.attr("kPixelFormat") = TraitsT::kPixelFormat;
+      ++i;
+
+      using ImageT = Image<Value>;
+      py::class_<ImageT> image(m, ("Image_" + names[i]))
+          .def(py::init<int, int>())
+          .def(py::init<int, int, T>())
+          .def("width", &ImageT::width)
+          .def("height", &ImageT::height)
+          .def("size", &ImageT::size)
+          .def("resize", &ImageT::resize)
+          .def("get", [](const ImageT* self, int x, int y) {
+                return *self->at(x, y);
+              })
+          .def("set", [](ImageT* self, int x, int y, T value) {
+                *self->at(x, y) = value;
+              })
+          .def("mutable_array", [](ImageT* self) {
+                return GetImageArray<ImageT, T>(self);
+              })
+          .def("array", [](const ImageT* self) {
+                return GetImageArray<const ImageT, const T>(self);
+              });
+      // Constants.
+      image.attr("Traits") = traits;
+    };
+    type_visit(iter, Pack{});
+  }
+
+  // Constants.
+  py::class_<InvalidDepth> invalid_depth(m, "InvalidDepth");
+  invalid_depth.attr("kTooFar") = InvalidDepth::kTooFar;
+  invalid_depth.attr("kTooClose") = InvalidDepth::kTooClose;
+
+  py::class_<Lable> label(m, "Label");
+  label.attr("kTooFar") = Label::kNoBody;
+  label.attr("kTooClose") = Label::kFlatTerrain;
 }
