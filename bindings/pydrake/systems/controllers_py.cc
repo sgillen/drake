709abd21b932c6cda808c23b89594d8e0d7357be
Eric Cousineau <eric.cousineau@tri.global>
2018-02-12 23:58:01 -0500

    controllers: Use WrapPtr to ensure Context<T> is not copied by pybind.

--- bindings/pydrake/systems/controllers_py.cc
+++ bindings/pydrake/systems/controllers_py.cc
@@ -4,10 +4,33 @@
 #include <pybind11/stl.h>
 
 #include "drake/bindings/pydrake/pydrake_pybind.h"
+#include "drake/bindings/pydrake/util/wrap_function.h"
 #include "drake/systems/controllers/dynamic_programming.h"
 
 namespace drake {
 namespace pydrake {
+
+namespace {
+
+template <typename T, typename = void>
+struct wrap_ptr : public wrap_arg_default<T> {};
+
+template <typename T>
+struct wrap_ptr<const systems::Context<T>&> {
+  using Type = systems::Context<T>;
+  static auto wrap(const Type& arg) { return &arg; }
+  static auto unwrap(const Type* arg_wrapped) { return *arg_wrapped; }
+};
+
+// Ensures that `const Context<T>&` is wrapped with `const Context<T>*`.
+// TODO(eric.cousineau): Replace this with general wrappper, place in
+// `pydrake_pybind` or somewhere related.
+template <typename Func>
+auto WrapPtr(Func&& func) {
+  return WrapFunction<wrap_ptr>(std::forward<Func>(func));
+}
+
+}  // namespace
 
 PYBIND11_MODULE(controllers, m) {
   // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
@@ -21,7 +44,7 @@ PYBIND11_MODULE(controllers, m) {
       .def_readwrite("discount_factor",
                     &DynamicProgrammingOptions::discount_factor);
 
-  m.def("FittedValueIteration", &FittedValueIteration);
+  m.def("FittedValueIteration", WrapPtr(&FittedValueIteration));
 }
 
 }  // namespace pydrake
