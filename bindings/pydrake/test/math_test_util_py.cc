480b79c304476635a4eaeb2a38e2a47d5dd5b6e2
Eric Cousineau <eric.cousineau@tri.global>
2018-04-09 18:34:01 -0400

    Fix linting

--- bindings/pydrake/test/math_test_util_py.cc
+++ bindings/pydrake/test/math_test_util_py.cc
@@ -12,30 +12,30 @@ namespace pydrake {
 namespace {
 
 class AutoDiffContainer {
-public:
-    AutoDiffContainer() {
-        value_.resize(1, 2);
-        value_ <<
-            AutoDiffXd(10, (VectorXd(2) << 1, 0).finished()),
-            AutoDiffXd(100, (VectorXd(2) << 0, 1).finished());
-    }
-    MatrixX<AutoDiffXd>& value() { return value_; }
-private:
-    MatrixX<AutoDiffXd> value_;
+ public:
+  AutoDiffContainer() {
+    value_.resize(1, 2);
+    value_ <<
+        AutoDiffXd(10, (VectorXd(2) << 1, 0).finished()),
+        AutoDiffXd(100, (VectorXd(2) << 0, 1).finished());
+  }
+  MatrixX<AutoDiffXd>& value() { return value_; }
+ private:
+  MatrixX<AutoDiffXd> value_;
 };
+
+}  // namespace
 
 PYBIND11_MODULE(math_test_util, m) {
   // Add utilities to test reference semantics for AutoDiff.
   py::class_<AutoDiffContainer>(m, "AutoDiffContainer")
       .def(py::init())
       .def("value", &AutoDiffContainer::value,
-           py::return_value_policy::reference_internal);
+           py_reference_internal);
 
   m.def("autodiff_increment", [](Eigen::Ref<MatrixX<AutoDiffXd>> value) {
       value.array() += 1;
   });
-}
-
 }
 
 }  // namespace pydrake
