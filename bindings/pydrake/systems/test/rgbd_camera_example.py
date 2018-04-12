6e7f6b3bc7dbdfec61621e907ac17e24c0b3c937
Eric Cousineau <eric.cousineau@tri.global>
2018-04-12 10:09:32 -0400

    Not sure why this stuff ain't showing up...

--- bindings/pydrake/systems/test/rgbd_camera_example.py
+++ bindings/pydrake/systems/test/rgbd_camera_example.py
@@ -1,4 +1,11 @@
 import numpy as np
+import time
+try:
+    import cv2
+except ImportError:
+    cv2 = None
+
+import sys; sys.stdout = sys.stderr
 
 from pydrake.common import FindResourceOrThrow
 from pydrake.multibody.rigid_body_tree import (
@@ -33,13 +40,13 @@ tree.addFrame(frame)
 camera = RgbdCamera(
     name="camera", tree=tree, frame=frame,
     z_near=0.5, z_far=5.0,
-    fov_y=np.pi / 4, show_window=False)
+    fov_y=np.pi / 4, show_window=True)
 
 print(camera.depth_camera_info().intrinsic_matrix())
 
 import subprocess
 subprocess.Popen(
-    "export -p | sed 's# PWD=# OLD_PWD=#g' > tee /tmp/env.sh",
+    "export -p | sed 's# PWD=# OLD_PWD=#g' > /tmp/env.sh",
     shell=True)
 
 # That we can access the state as images.
@@ -47,7 +54,16 @@ context = camera.CreateDefaultContext()
 x0 = np.zeros(tree.get_num_positions() + tree.get_num_velocities())
 context.FixInputPort(0, BasicVector(x0))
 output = camera.AllocateOutput(context)
-# camera.CalcOutput(context, output)
+camera.CalcOutput(context, output)
 depth_index = camera.depth_image_output_port().get_index()
-# depth_image = output.get_data(depth_index)
-# print(depth_image)
+depth_image = output.get_data(depth_index).get_value()
+print(depth_image)
+
+if cv2 is not None:
+    print(depth_image.data.shape)
+    print(depth_image.data.dtype)
+    print("Show cv2")
+    cv2.imshow("Depth Image", depth_image.data)
+    # In Bazel, we cannot connect to `stdin` due to the server/client running
+    # setup, so `cv.waitKey(...)` does not work. Sleep instead.
+    time.sleep(10)
