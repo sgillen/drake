37362e348184eb6553383ae1b834b05dc0536cd1
Eric Cousineau <eric.cousineau@tri.global>
2018-04-27 16:51:24 -0400

    pydrake wrap_pybind: Generalize pointer-wrapping for callback wrapping

--- bindings/pydrake/util/wrap_pybind.h
+++ bindings/pydrake/util/wrap_pybind.h
@@ -8,6 +8,8 @@
 
 #include "pybind11/pybind11.h"
 
+#include "drake/bindings/pydrake/util/wrap_function.h"
+
 namespace drake {
 namespace pydrake {
 
@@ -35,6 +37,31 @@ class MirrorDef {
   A* const a_{};
   B* const b_{};
 };
+
+namespace detail {
+
+template <typename T, typename = void>
+struct wrap_ref_ptr : public wrap_arg_default<T> {};
+
+// TODO(eric.cousineau): Only use this on non-primitive types (e.g. types whose
+// `type_caster`s are generic).
+template <typename T>
+struct wrap_ref_ptr<T&> {
+  static T* wrap(T& arg) { return &arg; }
+  static T& unwrap(T* arg_wrapped) { return *arg_wrapped; }
+};
+
+}  // namespace detail
+
+/// Ensures that any `T&` (which can infer for `T = const U`) is wrapped as
+/// `U*`. Most useful for functions which take `std::function<>` arguments with
+/// const or mutable references to objects that may only exist in C++, and not
+/// yet in Python.
+/// For more information, see: https://github.com/pybind/pybind11/issues/1241
+template <typename Func>
+auto WrapRefPtr(Func&& func) {
+  return WrapFunction<detail::wrap_ref_ptr>(std::forward<Func>(func));
+}
 
 }  // namespace pydrake
 }  // namespace drake
