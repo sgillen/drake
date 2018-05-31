9bb6dc0e3ecbffb9973c3aea6396c53ed13eb452
Eric Cousineau <eric.cousineau@tri.global>
2018-05-31 14:17:59 -0400

    Capture build

--- tmp/run.sh
+++ tmp/run.sh
@@ -5,15 +5,17 @@ prefix=$1
 
 cd $(dirname $0)/..
 
-bazel run --config=memcheck -c dbg //examples/kuka_iiwa_arm:kuka_simulation_test
-echo $?
-
 outdir=tmp/${prefix}
 mkdir -p ${outdir}
 
 filt() {
     sort | regex_sub.py '\(0x[\da-f]+\)' '' -
 }
+
+{
+    bazel run -s --config=memcheck -c dbg //examples/kuka_iiwa_arm:kuka_simulation_test
+    echo $?
+} > ${outdir}/build.txt 2>&1
 
 ldd bazel-bin/examples/kuka_iiwa_arm/kuka_simulation_test | filt > ${outdir}/bin.txt
 ldd bazel-bin/examples/kuka_iiwa_arm/../../_solib_k8/_U_S_Stools_Sinstall_Slibdrake_Cdrake_Ushared_Ulibrary___Utools_Sinstall_Slibdrake/libdrake.so | filt > ${outdir}/libdrake_install.txt
