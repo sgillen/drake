7c610ba32e6050b70e0f0367ed3a1bb8174a608c
Eric Cousineau <eric.cousineau@tri.global>
2018-06-12 15:38:21 -0400

    Segfault, but different location

--- systems/sensors/test/rgbd_renderer_ospray_test.cc
+++ systems/sensors/test/rgbd_renderer_ospray_test.cc
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <memory>
 
-#include "drake/systems/sensors/rgbd_renderer_ospray.h"
+#include <vtkAutoInit.h>
+#include <vtkNew.h>
+#include <vtkOSPRayPass.h>
+
+#include "drake/multibody/shapes/visual_element.h"
+
+VTK_MODULE_INIT(vtkRenderingOpenGL2)
 
 namespace drake {
 namespace systems {
 namespace sensors {
 namespace test {
 
-using Eigen::Isometry3d;
+void test_ospray(const DrakeShapes::VisualElement& visual) {
+  vtkNew<vtkOSPRayPass> ospray;
+  std::cerr << "ospray: " << &(*ospray) << std::endl;
+  const DrakeShapes::Geometry& geometry = visual.getGeometry();
+  unused(geometry);
+}
 
 int DoMain() {
-  std::unique_ptr<RgbdRendererOSPRay> renderer(new RgbdRendererOSPRay({}));
-  std::cerr << "renderer: " << renderer.get() << std::endl;
+  DrakeShapes::Sphere sphere(1.);
+  Eigen::Vector4d rgba;
+  rgba << 1, 0, 0, 1;
+  DrakeShapes::VisualElement element(
+    sphere, Eigen::Isometry3d::Identity(), rgba);
+
+  test_ospray(element);
+
   return 0;
 }
 
