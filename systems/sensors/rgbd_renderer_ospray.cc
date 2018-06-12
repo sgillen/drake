9c28257546a6619a147bdabda253323b1dd542e3
Eric Cousineau <eric.cousineau@tri.global>
2018-06-12 15:29:37 -0400

    Strip back down

--- systems/sensors/rgbd_renderer_ospray.cc
+++ systems/sensors/rgbd_renderer_ospray.cc
@@ -52,153 +52,26 @@ namespace drake {
 namespace systems {
 namespace sensors {
 
-using vtk_util::ConvertToVtkTransform;
-
-namespace {
-
-// TODO(kunimatsu-tri) Add support for the arbitrary clipping planes.
-const double kClippingPlaneNear = 0.01;
-const double kClippingPlaneFar = 100.;
-
-enum ImageType {
-  kColor = 0,
-};
-
-const int kNumOutputImage = 1;
-
-struct RenderingPipeline {
-  vtkNew<vtkRenderer> renderer;
-  vtkNew<vtkRenderWindow> window;
-  vtkNew<vtkWindowToImageFilter> filter;
-  vtkNew<vtkImageExport> exporter;
-};
-
-using ActorCollection = std::vector<vtkSmartPointer<vtkActor>>;
-
-std::string RemoveFileExtension(const std::string& filepath) {
-  const size_t last_dot = filepath.find_last_of(".");
-  if (last_dot == std::string::npos) {
-    DRAKE_ABORT_MSG("File has no extention.");
-  }
-  return filepath.substr(0, last_dot);
-}
-
-void SetModelTransformMatrixToVtkCamera(
-    vtkCamera* camera, const vtkSmartPointer<vtkTransform>& X_WC) {
-  // vtkCamera contains a transformation as the internal state and
-  // ApplyTransform multiplies a given transformation on top of the internal
-  // transformation. Thus, resetting 'Set{Position, FocalPoint, ViewUp}' is
-  // needed here.
-  camera->SetPosition(0., 0., 0.);
-  camera->SetFocalPoint(0., 0., 1.);  // Sets z-forward.
-  camera->SetViewUp(0., -1, 0.);  // Sets y-down. For the detail, please refer
-  // to CameraInfo's document.
-  camera->ApplyTransform(X_WC);
-}
-
-struct ModuleInitVtkRenderingOpenGL2 {
-  ModuleInitVtkRenderingOpenGL2() {
-    VTK_AUTOINIT_CONSTRUCT(vtkRenderingOpenGL2)
-  }
-};
-
-}  // namespace
-
-class RgbdRendererOSPRay::Impl : private ModuleInitVtkRenderingOpenGL2 {
+class RgbdRendererOSPRay::Impl {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(Impl)
 
   Impl(RgbdRendererOSPRay* parent, const Eigen::Isometry3d& X_WC);
   ~Impl() {}
 
-  optional<VisualIndex> ImplRegisterVisual(
-      const DrakeShapes::VisualElement& visual, int body_id);
-
  private:
 
   RgbdRendererOSPRay* parent_ = nullptr;
-  vtkNew<vtkLight> light_;
-  vtkNew<vtkOSPRayMaterialLibrary> materials_;
-  vtkNew<vtkActor> terrain_actor_;
-  // Use ImageType to access to this array. We assume pipelines_'s indices to be
-  // 0 for RGB, 1 for depth, and 2 for ground-truth label rendering.
-  std::array<std::unique_ptr<RenderingPipeline>,
-             kNumOutputImage> pipelines_;
-
-  // A map which takes pairs of a body index in RBT and three vectors of
-  // vtkSmartPointer to vtkActor for color, depth and label rendering
-  // respectively. Each vtkActor corresponds to an visual element specified in
-  // SDF / URDF.
-  std::map<int, std::array<ActorCollection, kNumOutputImage>> id_object_maps_;
 };
 
 RgbdRendererOSPRay::Impl::Impl(RgbdRendererOSPRay* parent,
                             const Eigen::Isometry3d& X_WC)
-    : parent_(parent),
-      pipelines_{{std::make_unique<RenderingPipeline>()}} {
-  auto& cp = pipelines_[ImageType::kColor];
-  if (parent_->config().show_window) {
-    cp->window->SetWindowName("Color Image");
-  } else {
-    for (auto& pipeline : pipelines_) {
-      pipeline->window->SetOffScreenRendering(1);
-    }
-  }
-
+    : parent_(parent) {
   // OSPRay specific configuration.
   vtkNew<vtkOSPRayPass> ospray;
+  unused(ospray);
+  unused(parent_);
   exit(0);
-  cp->renderer->SetPass(ospray);
-  vtkOSPRayRendererNode::SetRendererType("pathtracer", cp->renderer);
-  vtkOSPRayRendererNode::SetSamplesPerPixel(1, cp->renderer);
-
-  double np[3] = {0, 0, 1};
-  double ep[3] = {0, 1, 0};
-  vtkOSPRayRendererNode::SetNorthPole(np, cp->renderer);
-  vtkOSPRayRendererNode::SetEastPole(ep, cp->renderer);
-  vtkOSPRayRendererNode::SetMaterialLibrary(materials_, cp->renderer);
-
-  const vtkSmartPointer<vtkTransform> vtk_X_WC = ConvertToVtkTransform(X_WC);
-
-  for (auto& pipeline : pipelines_) {
-    auto camera = pipeline->renderer->GetActiveCamera();
-    camera->SetViewAngle(parent_->config().fov_y * 180. / M_PI);
-    camera->SetClippingRange(kClippingPlaneNear, kClippingPlaneFar);
-    SetModelTransformMatrixToVtkCamera(camera, vtk_X_WC);
-    pipeline->window->SetSize(parent_->config().width,
-                              parent_->config().height);
-    pipeline->window->AddRenderer(pipeline->renderer);
-    pipeline->filter->SetInput(pipeline->window);
-    pipeline->filter->SetScale(1);
-    pipeline->filter->ReadFrontBufferOff();
-    pipeline->filter->SetInputBufferTypeToRGBA();
-    pipeline->filter->Update();
-    pipeline->exporter->SetInputData(pipeline->filter->GetOutput());
-    pipeline->exporter->ImageLowerLeftOff();
-  }
-
-  // TODO(kunimatsu-tri) Add API to handle lighting stuff.
-  light_->SetPosition(-2, 0, 10);
-  light_->SetFocalPoint(0, 0, 0);
-  light_->PositionalOff();
-  // OSPRay specific control, radius to get soft shadows.
-  vtkOSPRayLightNode::SetRadius(2.0, light_);
-  light_->SetTransformMatrix(vtk_X_WC->GetMatrix());
-
-  cp->renderer->AddLight(light_);
-}
-
-optional<RgbdRenderer::VisualIndex>
-RgbdRendererOSPRay::Impl::ImplRegisterVisual(
-    const DrakeShapes::VisualElement& visual, int body_id) {
-  std::array<vtkNew<vtkActor>, kNumOutputImage> actors;
-  std::array<vtkNew<vtkPolyDataMapper>, kNumOutputImage> mappers;
-
-  bool shape_matched = true;
-//  const DrakeShapes::Geometry& geometry = visual.getGeometry();
-
-  unused(RemoveFileExtension, shape_matched); //, geometry);
-  return nullopt;
 }
 
 RgbdRendererOSPRay::RgbdRendererOSPRay(const RenderingConfig& config,
