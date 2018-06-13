8eb084bde8df09d5641d2577da21345be8087a2a
Eric Cousineau <eric.cousineau@tri.global>
2018-06-13 01:48:08 -0400

    Make more succinct

--- repro.sh
+++ repro.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
-set -x
+set -eux
 
+use_cmake=1
+
+cd $(dirname $0)
+
+if [[ ${use_cmake} -eq 1 ]]; then
+    ./tmp/build_cmake.sh
+    bin=tmp/build/rgbd_ospray_minimal
+else
+    bazel build --compiler=gcc-5 -c dbg //tmp:rgbd_ospray_minimal
+    bin=bazel-bin/tmp/rgbd_ospray_minimal
+fi
+
+if [[ $(sysctl kernel.core_pattern) != "kernel.core_pattern = /tmp/core_dump" ]]; then
+    echo 'Please run `sysctl -w kernel.core_pattern=/tmp/core_dump` for debugging.'
+    exit 1;
+fi
+ulimit -c unlimited
+
+rm -f /tmp/core_dump
+
+set +e
+(
+    set -e
+    # Normally takes about ~30 executions to produce segfault. Can range
+    # anywhere from 2-50 (or more).
+    for i in $(seq 300); do
+        echo "[ $i ]"
+        ${bin}
+    done
+)
 set -e
-bazel build --compiler=gcc-5 -c dbg //systems/sensors:rgbd_renderer_ospray_test
-bazel-bin/systems/sensors/rgbd_renderer_ospray_test
 
-# bazel build --compiler=clang-4.0 -c dbg //systems/sensors:rgbd_renderer_ospray_test
-# gdb -ex run bazel-bin/systems/sensors/rgbd_renderer_ospray_test
+# Postmortem
+gdb -ex bt -ex q ${bin} /tmp/core_dump
--- tmp/build_cmake.sh
+++ tmp/build_cmake.sh
@@ -4,7 +4,7 @@ set -eux
 cd $(dirname $0)
 bazel build --compiler=gcc-5 -c dbg @vtk//:vtk
 
-prefix_dir=${PWD}/../bazel-drake/external/vtk
+prefix_dir=$(cd ../bazel-drake/external/vtk && pwd)
 test -d ${prefix_dir}
 
 rm -rf build && mkdir build && cd build
@@ -12,6 +12,7 @@ rm -rf build && mkdir build && cd build
 # Symlink for datafiles.
 ln -s .. tmp
 
-cmake -DCMAKE_PREFIX_PATH=${prefix_dir} -DCMAKE_BUILD_TYPE=Debug ..
+cmake .. \
+    -DCMAKE_PREFIX_PATH=${prefix_dir} \
+    -DCMAKE_BUILD_TYPE=Debug
 make -j
-./OSPRayPathTracer
