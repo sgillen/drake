9079820b57e32575d7d4b1e66c8b5d49bea13297
Eric Cousineau <eric.cousineau@tri.global>
2018-09-06 20:44:33 -0400

    Random fixes

--- bindings/pydrake/multibody/test/inverse_kinematics_test.py
+++ bindings/pydrake/multibody/test/inverse_kinematics_test.py
@@ -1,6 +1,9 @@
 from pydrake.multibody import inverse_kinematics as ik
 
 import math
+import unittest
+
+import numpy as np
 
 from pydrake.multibody.multibody_tree.multibody_plant import MultibodyPlant 
 from pydrake.multibody.multibody_tree.parsing import (
@@ -9,7 +12,6 @@ from pydrake.multibody.multibody_tree.parsing import (
 from pydrake.multibody.benchmarks.acrobot import (
     MakeAcrobotPlant,
 )
-import unittest
 import pydrake.solvers.mathematicalprogram as mp
 
 from pydrake.common import FindResourceOrThrow
@@ -24,21 +26,23 @@ class TestInverseKinematics(unittest.TestCase):
         self.link1_frame = self.plant.GetBodyByName("body1").body_frame()
         self.link2_frame = self.plant.GetBodyByName("body2").body_frame()
         self.ik_two_bodies= ik.InverseKinematics(self.plant)
+        self.prog = self.ik_two_bodies.get_mutable_prog()
+        self.q = self.ik_two_bodies.q()
 
         def squaredNorm(x):
             return np.array([x[0] * x[0] + x[1] * x[1] + x[2] * x[2] + x[3] * x[3]])
 
-        self.ik_two_bodies.get_mutable_prog().AddConstraint(squaredNorm, [1], [1], self.ik_two_bodies.q()[:4])
-        self.ik_two_bodies.get_mutable_prog().AddConstraint(squaredNorm, [1], [1], self.ik_two_bodies.q()[7:11])
+        self.prog.AddConstraint(squaredNorm, [1], [1], self.q[:4])
+        self.prog.AddConstraint(squaredNorm, [1], [1], self.q[7:11])
 
     def test_AddPositionConstraint(self):
         self.ik_two_bodies.AddPositionConstraint(self.link1_frame, [0, 0.1, 0.2], self.link2_frame, [-1, -1, -1], [1, 1, 1])
-        result = self.ik_two_bodies.get_mutable_prog().Solve()
+        result = self.prog.Solve()
         self.assertEqual(result, mp.SolutionResult.kSolutionFound)
-        q_val = self.ik_two_bodies.prog().GetSolution(self.ik_two_bodies.q())
+        q_val = prog.GetSolution(self.q)
         print q_val
 
     def test_AddOrientationConstraint(self):
         self.ik_two_bodies.AddOrientationConstraint(self.link1_frame, self.link2_frame, 0.5 * math.pi)
-        result = self.ik_two_bodies.get_mutable_prog().Solve()
+        result = self.prog.Solve()
         self.assertEqual(result, mp.SolutionResult.kSolutionFound)
