580039dfb55ee67078177f18fecb2cb0bd6cb364
Eric Cousineau <eric.cousineau@tri.global>
2018-09-26 13:37:39 -0400

    Scriptify

---
+++ third_party/com_github_pybind_pybind11/tmp_sub_doc.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import re
+from common_scripts.text_processor import TextProcessor
+
+class Custom(TextProcessor):
+    def __init__(self):
+        TextProcessor.__init__(self, skipArgs = True, description = "Apply python regex to file")
+        # self.parser.add_argument('pattern', type = str)
+        # self.parser.add_argument('replace', type = str)
+        self.addArguments()
+        
+    def process(self, oldText):
+        define_pattern = r"#define D\(...\) DOC\((.*?), __VA_ARGS__\)"
+
+        def define_sub(m):
+            pieces = m.group(1).split(", ")
+            return "auto& doc = pydrake_doc." + ".".join(pieces) + ";"
+
+        usage_pattern = r"\bD\((.*?)\)"
+
+        def usage_sub(m):
+            pieces = m.group(1).split(", ")
+            suffix = ""
+            try:
+                index = int(pieces[-1])
+                pieces = pieces[:-1]
+                suffix = "_{}".format(index)
+            except ValueError:
+                pass
+            return "doc." + ".".join(pieces) + ".doc" + suffix
+
+        new = oldText
+        new = re.sub(define_pattern, define_sub, new, re.MULTILINE | re.DOTALL)
+        new = re.sub(usage_pattern, usage_sub, new, re.MULTILINE | re.DOTALL)
+        return new
+
+Custom().main()
