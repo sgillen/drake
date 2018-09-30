5b86abb9212df9f4ec3fb49d7928e383991479e4
Eric Cousineau <eric.cousineau@tri.global>
2018-09-29 21:32:42 -0400

    Fix inference (was just using Class& instead of Class*)

--- bindings/pydrake/util/wrap_function.h
+++ bindings/pydrake/util/wrap_function.h
@@ -4,92 +4,107 @@
 #include <type_traits>
 #include <utility>
 
+#include "drake/bindings/pydrake/util/type_pack.h"
+
 namespace drake {
 namespace pydrake {
 namespace detail {
 
-// Collects both a functor object and its signature for ease of inference.
-template <typename FuncT, typename ReturnT, typename ... ArgsT>
-struct function_info {
-  // TODO(eric.cousineau): Ensure that this permits copy elision when combined
-  // with `std::forward<Func>(func)`, while still behaving well with primitive
-  // types.
-  using Func = FuncT;
-  std::decay_t<Func> func;
+struct function_inference {
+  // Collects both a functor object and its signature for ease of inference.
+  template <typename Func_, typename ReturnT, typename ... ArgsT>
+  struct info {
+    // TODO(eric.cousineau): Ensure that this permits copy elision when combined
+    // with `std::forward<Func>(func)`, while still behaving well with primitive
+    // types.
+    using Return = ReturnT;
+    using Args = type_pack<ArgsT...>;
+    using Func = std::decay_t<Func_>;
+    Func func;
+  };
 
-  using Return = ReturnT;
-  using Args = type_pack<ArgsT...>;
+  // Factory method for `info<>`, to be used by `run`.
+  template <typename Return, typename ... Args, typename Func>
+  static auto make_inferred_info(
+      Func&& func, Return (*infer)(Args...) = nullptr) {
+    (void)infer;
+    return info<Func, Return, Args...>{std::forward<Func>(func)};
+  }
+
+  // Infers `info<>` from a function pointer.
+  template <typename Return, typename ... Args>
+  static auto run(Return (*func)(Args...)) {
+    return make_inferred_info<Return, Args...>(func);
+  }
+
+  // Infers `info<>` from a mutable method pointer.
+  template <typename Return, typename Class, typename ... Args>
+  static auto run(Return (Class::*method)(Args...)) {
+    auto func = [method](Class& self, Args... args) {
+      return (self.*method)(std::forward<Args>(args)...);
+    };
+    return make_inferred_info<Return, Class&, Args...>(func);
+  }
+
+  // Infers `info<>` from a const method pointer.
+  template <typename Return, typename Class, typename ... Args>
+  static auto run(Return (Class::*method)(Args...) const) {
+    auto func = [method](const Class& self, Args... args) {
+      return (self.*method)(std::forward<Args>(args)...);
+    };
+    return make_inferred_info<Return, const Class&, Args...>(func);
+  }
+
+  // Helpers for general functor objects.
+  struct infer_helper {
+    // Removes class from mutable method pointer for inferring signature
+    // of functor.
+    template <typename Class, typename Return, typename ... Args>
+    static auto remove_class_from_ptr(Return (Class::*)(Args...)) {
+      using Ptr = Return (*)(Args...);
+      return Ptr{};
+    }
+
+    // Removes class from const method pointer for inferring signature of
+    // functor.
+    template <typename Class, typename Return, typename ... Args>
+    static auto remove_class_from_ptr(Return (Class::*)(Args...) const) {
+      using Ptr = Return (*)(Args...);
+      return Ptr{};
+    }
+
+    // Infers funtion pointer from functor.
+    // @pre `Func` must have only *one* overload of `operator()`.
+    template <typename Func>
+    static auto infer_function_ptr() {
+      return remove_class_from_ptr(&Func::operator());
+    }
+  };
+
+  // SFINAE for functors.
+  // N.B. This *only* distinguished between function / method pointers and
+  // lambda objects. It does *not* distinguish among other types.
+  template <typename Func, typename T = void>
+  using enable_if_lambda_t =
+      std::enable_if_t<!std::is_function<std::decay_t<Func>>::value, T>;
+
+  // Infers `info<>` from a generic functor.
+  template <typename Func, typename = enable_if_lambda_t<Func>>
+  static auto run(Func&& func) {
+    return make_inferred_info(
+        std::forward<Func>(func),
+        infer_helper::infer_function_ptr<std::decay_t<Func>>());
+  }
 };
 
-// Factory method for `function_info<>`, to be used by `infer_function_info`.
-template <typename Return, typename ... Args, typename Func>
-auto make_function_info(Func&& func, Return (*infer)(Args...) = nullptr) {
-  (void)infer;
-  return function_info<Func, Return, Args...>{std::forward<Func>(func)};
-}
-
-// SFINAE for functors.
-// N.B. This *only* distinguished between function / method pointers and
-// lambda objects. It does *not* distinguish among other types.
-template <typename Func, typename T = void>
-using enable_if_lambda_t =
-    std::enable_if_t<!std::is_function<std::decay_t<Func>>::value, T>;
-
-// Infers `function_info<>` from a function pointer.
-template <typename Return, typename ... Args>
-auto infer_function_info(Return (*func)(Args...)) {
-  return make_function_info<Return, Args...>(func);
-}
-
-// Infers `function_info<>` from a mutable method pointer.
-template <typename Return, typename Class, typename ... Args>
-auto infer_function_info(Return (Class::*method)(Args...)) {
-  auto func = [method](Class* self, Args... args) {
-    return (self->*method)(std::forward<Args>(args)...);
-  };
-  return make_function_info<Return, Class*, Args...>(func);
-}
-
-// Infers `function_info<>` from a const method pointer.
-template <typename Return, typename Class, typename ... Args>
-auto infer_function_info(Return (Class::*method)(Args...) const) {
-  auto func = [method](const Class* self, Args... args) {
-    return (self->*method)(std::forward<Args>(args)...);
-  };
-  return make_function_info<Return, const Class*, Args...>(func);
-}
-
-// Helpers for general functor objects.
-struct functor_helpers {
-  // Removes class from mutable method pointer for inferring signature
-  // of functor.
-  template <typename Class, typename Return, typename ... Args>
-  static auto remove_class_from_ptr(Return (Class::*)(Args...)) {
-    using Ptr = Return (*)(Args...);
-    return Ptr{};
-  }
-
-  // Removes class from const method pointer for inferring signature of functor.
-  template <typename Class, typename Return, typename ... Args>
-  static auto remove_class_from_ptr(Return (Class::*)(Args...) const) {
-    using Ptr = Return (*)(Args...);
-    return Ptr{};
-  }
-
-  // Infers function pointer from functor.
-  // @pre `Func` must have only *one* overload of `operator()`.
-  template <typename Func>
-  static auto infer_function_ptr() {
-    return remove_class_from_ptr(&Func::operator());
-  }
-};
-
-// Infers `function_info<>` from a generic functor.
-template <typename Func, typename = detail::enable_if_lambda_t<Func>>
+// Infers `function_inference::info<>` from a generic functor, converting form
+// the left form to the right form:
+//   <lambda>  ->  Return(*)(Args...)
+//   Return (Class::*)(Args...)  ->  Return (*)(Class&, Args...)
+//   Return (Class::*)(Args...) const  ->  Return (*)(const Class&, Args...)
+template <typename Func>
 auto infer_function_info(Func&& func) {
-  return make_function_info(
-      std::forward<Func>(func),
-      functor_helpers::infer_function_ptr<std::decay_t<Func>>());
+  return function_inference::run(std::forward<Func>(func));
 }
 
 // Implementation for wrapping a function by scanning and replacing arguments
@@ -179,7 +194,7 @@ struct wrap_function_impl {
   // Wraps function arguments and the return value.
   // Generally used when `Return` is non-void.
   template <typename Func, typename Return, typename ... Args>
-  static auto run(function_info<Func, Return, Args...>&& info,
+  static auto run(function_inference::info<Func, Return, Args...>&& info,
       std::enable_if_t<enable_wrap_output<Return>, void*> = {}) {
     // N.B. Since we do not use the `mutable` keyword with this lambda,
     // any functors passed in *must* provide `operator()(...) const`.
@@ -196,7 +211,7 @@ struct wrap_function_impl {
   // Wraps function arguments, but not the return value.
   // Generally used when `Return` is void.
   template <typename Func, typename Return, typename ... Args>
-  static auto run(function_info<Func, Return, Args...>&& info,
+  static auto run(function_inference::info<Func, Return, Args...>&& info,
       std::enable_if_t<!enable_wrap_output<Return>, void*> = {}) {
     auto func_wrapped =
         [func_f = std::forward<Func>(info.func)]
