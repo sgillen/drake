50f0684b02839a0f8f8d9d2c2039e35b415e7efc
Eric Cousineau <eric.cousineau@tri.global>
2018-09-29 22:12:44 -0400

    Split up things a bit more

--- bindings/pybind11_ext/inference.h
+++ bindings/pybind11_ext/inference.h
@@ -1,23 +1,14 @@
 #pragma once
 
 #include "drake/bindings/pydrake/util/type_pack.h"
-#include "drake/bindings/pydrake/util/wrap_function.h"
+#include "drake/bindings/pydrake/util/function_inference.h"
 
 namespace pybind11 {
 namespace detail {
 
 using drake::type_pack;
-
-template <typename... A, typename... B>
-auto type_pack_concat(type_pack<A...> = {}, type_pack<B...> = {}) {
-  return type_pack<A..., B...>{};
-}
-
-template <template <typename> class Apply, typename... T>
-auto type_pack_apply(type_pack<T...> = {}) {
-  return type_pack<Apply<T>...>{};
-}
-
+using drake::type_pack_apply;
+using drake::type_pack_concat;
 using drake::pydrake::detail::infer_function_info;
 
 }  // namespace detail
