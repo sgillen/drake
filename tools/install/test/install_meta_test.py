5a70dc1059bf0e589be1fe7960d39183cac2a9ce
Eric Cousineau <eric.cousineau@tri.global>
2018-12-07 18:59:40 -0500

    Simplify testing process

--- tools/install/test/install_meta_test.py
+++ tools/install/test/install_meta_test.py
@@ -1,3 +1,5 @@
+"""Tests the behavior of targets generated from `install.py.in`."""
+
 import os
 from os.path import isdir, isfile, join
 from shutil import rmtree
@@ -9,21 +11,19 @@ import sys
 class TestInstallMeta(unittest.TestCase):
     BINARY = "tools/install/dummy/dummy_install"
 
-    def setUp(self):
+    def get_install_dir(self, case):
+        # Do not use `install_test_helper`, as its behavior is more constrained
+        # than what is useful for these tests.
         assert "TEST_TMPDIR" in os.environ, (
             "Must only be run within `bazel test`.")
-
-    def get_python_site_packages_dir(self):
-        major, minor = sys.version_info.major, sys.version_info.minor
-        return join("lib", "python{}.{}".format(major, minor), "site-packages")
-
-    def get_install_dir(self, case):
-        # Do not use `install_test_helper`, as its behavior more constrained
-        # than what is useful for these tests.
         install_dir = join(os.environ["TEST_TMPDIR"], "installation", case)
         # Ensure this is only called once per case.
         self.assertFalse(isdir(install_dir), case)
         return install_dir
+
+    def get_python_site_packages_dir(self):
+        major, minor = sys.version_info.major, sys.version_info.minor
+        return join("lib", "python{}.{}".format(major, minor), "site-packages")
 
     def test_nominal(self):
         """Test nominal behavior of install."""
@@ -45,16 +45,23 @@ class TestInstallMeta(unittest.TestCase):
         # Negative test: Ensure `--disable_nostrip_warning` is not used in a
         # blanket fashion.
         returncode = call(
-            [self.BINARY, install_dir, "--disable_nostrip_warning"])
+            [self.BINARY, install_dir, "--disable_no_strip_warning"])
         self.assertEqual(returncode, 1)
         # Test for warnings with no stripping:
-        warning_substr = "WARNING: Symbols are not stripped"
+        strip_substr = "NOTE: Symbols are not stripped."
+        warning_substr = "WARNING: Proprietary dependencies are enabled."
         # - Nominal.
         text_with_warning = check_output([
             self.BINARY, install_dir, "--no_strip"], stderr=STDOUT)
-        self.assertIn(warning_substr, text_with_warning, text_with_warning)
+        # N.B. `assertIn` error messages are not great for multiline, so just
+        # print and use nominal asserts.
+        print(text_with_warning)
+        self.assertTrue(strip_substr in text_with_warning)
+        self.assertTrue(warning_substr in text_with_warning)
         # - Warning disabled.
         text_without_warning = check_output([
             self.BINARY, install_dir, "--no_strip",
-            "--disable_nostrip_warning"], stderr=STDOUT)
-        self.assertNotIn(warning_substr, text_without_warning)
+            "--disable_no_strip_warning"], stderr=STDOUT)
+        print(text_without_warning)
+        self.assertTrue(strip_substr in text_without_warning)
+        self.assertTrue(warning_substr not in text_without_warning)
