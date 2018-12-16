154a885759441e73c97ea08788c982bb8db07dfd
Eric Cousineau <eric.cousineau@tri.global>
2018-12-16 13:46:43 -0500

    More scrub, more strings

--- issue_10244_repro.sh
+++ issue_10244_repro.sh
@@ -12,13 +12,13 @@ set -e
 
 _bazel_out=$(readlink -f ./bazel-out)
 normalize() {
-    sed -r -e "s#${PWD}#\$PWD#g" -e 's#0x[0-9a-g]+#[address]#g' -e "s#${_bazel_out}#\$_bazel_out#g"
+    sed -r -e "s#${PWD}#\${PWD}#g" -e 's#0x[0-9a-g]+#[address]#g' -e "s#${_bazel_out}#\${PWD}/bazel-out#g"
 }
 
 ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static | sort | normalize > ldd_static.txt
 ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | sort | normalize > ldd_shared.txt
 
 set +e
-strace ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static 2>&1 | normalize | tee strace_static.txt
-strace ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared 2>&1 | normalize | tee strace_shared.txt
+env -i strace -s 256 ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static 2>&1 | normalize | tee strace_static.txt
+env -i strace -s 256 ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared 2>&1 | normalize | tee strace_shared.txt
 set -e
