c1c47c36231dd51ecb4f4d0cf68418ce302f8848
Eric Cousineau <eric.cousineau@tri.global>
2018-12-16 12:45:28 -0500

    The following is OK

--- issue_10244_repro.sh
+++ issue_10244_repro.sh
@@ -5,8 +5,12 @@ bazel build \
     //bindings/pydrake/systems:issue_10255_min_repro_static \
     //bindings/pydrake/systems:issue_10255_min_repro_shared
 
-./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static
-./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared
+./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static | tee output_static.txt
+./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | tee output_shared.txt
 
-ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static | sort | sed "s#${PWD}#\$PWD#g" > ldd_static.txt
-ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | sort | sed "s#${PWD}#\$PWD#g" > ldd_shared.txt
+normalize() {
+    sort | sed -r -e "s#${PWD}#\$PWD#g" -e 's#\(0x[0-9a-g]+\)#([address])#g'
+}
+
+ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static | normalize > ldd_static.txt
+ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | normalize > ldd_shared.txt
