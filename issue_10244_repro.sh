02cf96a1a97046a495bd88dd4c645f326c0efeb8
Eric Cousineau <eric.cousineau@tri.global>
2018-12-16 12:47:07 -0500

    ospray common is what does it...

--- issue_10244_repro.sh
+++ issue_10244_repro.sh
@@ -5,8 +5,10 @@ bazel build \
     //bindings/pydrake/systems:issue_10255_min_repro_static \
     //bindings/pydrake/systems:issue_10255_min_repro_shared
 
+set +e
 ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static | tee output_static.txt
 ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | tee output_shared.txt
+set -e
 
 normalize() {
     sort | sed -r -e "s#${PWD}#\$PWD#g" -e 's#\(0x[0-9a-g]+\)#([address])#g'
