09468f69a8cb3400d1587cf9e12343c5355c2eee
Eric Cousineau <eric.cousineau@tri.global>
2018-12-16 13:14:32 -0500

    strace of offending code

--- issue_10244_repro.sh
+++ issue_10244_repro.sh
@@ -10,9 +10,15 @@ set +e
 ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | tee output_shared.txt
 set -e
 
+_bazel_out=$(readlink -f ./bazel-out)
 normalize() {
-    sort | sed -r -e "s#${PWD}#\$PWD#g" -e 's#\(0x[0-9a-g]+\)#([address])#g'
+    sed -r -e "s#${PWD}#\$PWD#g" -e 's#0x[0-9a-g]+#[address]#g' -e "s#${_bazel_out}#\$_bazel_out#g"
 }
 
-ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static | normalize > ldd_static.txt
-ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | normalize > ldd_shared.txt
+ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static | sort | normalize > ldd_static.txt
+ldd bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared | sort | normalize > ldd_shared.txt
+
+set +e
+strace ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_static 2>&1 | normalize | tee strace_static.txt
+strace ./bazel-bin/bindings/pydrake/systems/issue_10255_min_repro_shared 2>&1 | normalize | tee strace_shared.txt
+set -e
