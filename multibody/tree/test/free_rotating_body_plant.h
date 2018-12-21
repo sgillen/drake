3e9e0105f9c8f9bcefcf090374364880389ae3a0
Eric Cousineau <eric.cousineau@tri.global>
2018-12-21 17:02:09 -0500

    moar

--- multibody/tree/test/free_rotating_body_plant.h
+++ multibody/tree/test/free_rotating_body_plant.h
@@ -29,7 +29,7 @@ namespace test {
 /// They are already available to link against in the containing library.
 /// No other values for T are currently supported.
 template<typename T>
-class FreeRotatingBodyPlant final : public MultibodyTreeSystem<T> {
+class FreeRotatingBodyPlant final : public internal::MultibodyTreeSystem<T> {
  public:
   DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(FreeRotatingBodyPlant)
 
@@ -74,7 +74,7 @@ class FreeRotatingBodyPlant final : public MultibodyTreeSystem<T> {
   /// SetDefaultState(). Currently a non-zero value.
   Vector3<double> get_default_initial_angular_velocity() const;
 
-  using MultibodyTreeSystem<T>::tree;
+  using internal::MultibodyTreeSystem<T>::tree;
 
  private:
   void DoCalcTimeDerivatives(
@@ -98,7 +98,7 @@ class FreeRotatingBodyPlant final : public MultibodyTreeSystem<T> {
   double J_{0};
 
   const RigidBody<T>* body_{nullptr};
-  const SpaceXYZMobilizer<T>* mobilizer_{nullptr};
+  const internal::SpaceXYZMobilizer<T>* mobilizer_{nullptr};
 };
 
 }  // namespace test
