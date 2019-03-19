a1fdc088de6888bd82c375e63b4cf6fd87bfd3b7
Eric Cousineau <eric.cousineau@tri.global>
2019-03-19 09:49:40 -0400

    Make more minimal repro

--- tmp/director_marker_repro.py
+++ tmp/director_marker_repro.py
@@ -2,13 +2,8 @@
 To run:
 
     cd drake
-    # Terminal 1
     bazel run //tools:drake_visualizer -- \
         --script ${PWD}/tmp/director_marker_repro.py
-
-    # Terminal 2
-    bazel run //manipulation/util:show_model -- \
-        ${PWD}/examples/kuka_iiwa_arm/models/table/extra_heavy_duty_table_surface_only_collision.sdf
 """
 
 from director import affordancemanager
