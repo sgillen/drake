b9d060b5d09d1858cbcabed585078f1984fd3434
Eric Cousineau <eric.cousineau@tri.global>
2019-07-30 17:46:32 -0400

    Yup, that's it

--- bindings/pydrake/systems/framework_py.cc
+++ bindings/pydrake/systems/framework_py.cc
@@ -1,7 +1,7 @@
 #include "drake/bindings/pydrake/pydrake_pybind.h"
-#include "drake/bindings/pydrake/systems/framework_py_semantics.h"
-#include "drake/bindings/pydrake/systems/framework_py_systems.h"
-#include "drake/bindings/pydrake/systems/framework_py_values.h"
+// #include "drake/bindings/pydrake/systems/framework_py_semantics.h"
+// #include "drake/bindings/pydrake/systems/framework_py_systems.h"
+// #include "drake/bindings/pydrake/systems/framework_py_values.h"
 
 namespace drake {
 namespace pydrake {
@@ -10,13 +10,16 @@ PYBIND11_MODULE(framework, m) {
   PYDRAKE_PREVENT_PYTHON3_MODULE_REIMPORT(m);
   m.doc() = "Bindings for the core Systems framework.";
 
-  // Import autodiff and symbolic modules so that their types are declared for
-  // use as template parameters.
-  py::module::import("pydrake.autodiffutils");
-  py::module::import("pydrake.symbolic");
+  // // Import autodiff and symbolic modules so that their types are declared for
+  // // use as template parameters.
+  // py::module::import("pydrake.autodiffutils");
+  // py::module::import("pydrake.symbolic");
+
+  py::module py_framework = py::module::import("pydrake.systems.framework");
+  py::print(py_framework);
 
   // Incorporate definitions as pieces (to speed up compilation).
-  DefineFrameworkPyValues(m);
+  // DefineFrameworkPyValues(m);
   // DefineFrameworkPySemantics(m);
   // DefineFrameworkPySystems(m);
 }
