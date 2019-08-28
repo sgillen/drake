7deef3814ba306ba5afe982456612fb6fc219023
Eric Cousineau <eric.cousineau@tri.global>
2019-08-28 17:20:49 -0400

    Python 2: Getting some dumb stuff...

--- bindings/pydrake/test/math_test.py
+++ bindings/pydrake/test/math_test.py
@@ -113,6 +113,16 @@ class TestMath(unittest.TestCase):
         if T == Expression:
             # Pickling not enabled for Expression.
             return
+
+        # HACK: numpy array?
+        x = np.array([1, 2, 3])
+        g = BytesIO()
+        pickle.dump(x, g)
+        g.seek(0)
+        y = pickle.load(g)
+        print(y)
+        numpy_compare.assert_equal(x, y)
+
         f = BytesIO()
         pickle.dump(input, f)
         f.seek(0)
