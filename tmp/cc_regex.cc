5d4c6b219b1d51607764840e345040c6a23a990f
Eric Cousineau <eric.cousineau@tri.global>
2019-09-24 11:24:23 -0400

    Indicate usage of cxx11 abi

--- tmp/cc_regex.cc
+++ tmp/cc_regex.cc
@@ -7,8 +7,10 @@ namespace drake {
 
 std::string GetName() {
   // Construct a regex to try and repro `free(): invalid pointer`
+  std::cerr
+      << "_GLIBCXX_USE_CXX11_ABI: " << _GLIBCXX_USE_CXX11_ABI << std::endl;
   std::regex sub("\\b(class|struct|enum|union) ");
-  std::cout << &sub << std::endl;
+  std::cerr << "&sub: " << &sub << std::endl;
   return "nothing";
 }
 
